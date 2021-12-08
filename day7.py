dadddb22a24771c34c641ce8959487bd7fd8922a
Chris Last <clast@qti.qualcomm.com>
2021-12-08 00:31:47 +0000

    day7

--- day7.py
+++ day7.py
@@ -31,15 +31,55 @@ def p1(expect=349812, viz=None):
 
 
 ######## Part 2 ##########
-def p2(expect=1632146183902, viz=None):
-    pass
+def fuelcalc(crabs, pos):
+    acc = 0
+    for cpos, n in crabs.items():
+        acc += sum(range(abs(cpos-pos)+1)) * n
+    return acc
 
+def p2(expect=99763899, viz=None):
+    scatter = []
+    data = dict(Counter(map(int, PARSED)))
+    imax = max(data)
+    imin = min(data)
+    iguess = (imax + imin) // 2
+    newval = 0
+    while True:
+        oldval = newval
+        newval = fuelcalc(data, iguess)
+        scatter.append((iguess, newval))
+        if newval > oldval:
+            break
+        iguess += 1
+    while True:
+        oldval = newval
+        newval = fuelcalc(data, iguess)
+        scatter.append((iguess, newval))
+        if newval > oldval:
+            break
+        iguess -= 1
+    if viz:
+        viz(scatter)
+    return oldval
 
 ######### Main ###########
 def main():
     show(p1, p2)
 
+def viz2(scatter):
+    fig, ax = plt.subplots()
+    xdata = [_[0] for _ in scatter]
+    ydata = [_[1] for _ in scatter]
+    ax.plot(xdata, ydata)
+    ax.set_xlabel("crab submarine position")
+    ax.set_ylabel("fuel used")
+    fig.set_tight_layout(True)
+    full = Path(__file__).parent / 'output' / 'day7b.png'
+    fig.savefig(full)
+    img = Image.open(full)
+    img.resize((100,100)).save(Path(__file__).parent / 'output' / 'day7bthumb.png')
+
 if __name__ == "__main__":
     main()
     #p1(viz=viz1)
-    #p2(viz=viz2)
+    p2(viz=viz2)
