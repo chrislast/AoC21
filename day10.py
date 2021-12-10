503c960a75ff8a9675d70629aec674e542018acb
Chris Last <clast@qti.qualcomm.com>
2021-12-10 12:03:52 +0000

    readability + use set intersection to find list of chars in text instead of any([generator])

--- day10.py
+++ day10.py
@@ -1,52 +1,59 @@
 # import our helpers
-from utils import load, show, day, TRACE, Map, Path
+from utils import load, show, day
 
 ####### GLOBALS #########
 
 # load todays input data as a docstring
 DATA = load(day(__file__)).splitlines()
-
-# parse the input
-
-######## Part 1 ##########
 # syntax checker scores
 ERRVAL = {")": 3, "]": 57, "}": 1197, ">": 25137}
+# line completer scores
+COMPVAL = {")": 1, "]": 2, "}": 3, ">": 4}
 
 def trim(txt):
-    """
-    eliminate empty matching braces until they're all gone.
-    - used for both parts
-    """
+    """remove empty braces until done"""
     while True:
-        edited = txt.replace("{}","").replace("[]","").replace("<>","").replace("()","")
+        edited = txt.replace(
+            "{}","").replace(
+            "[]","").replace(
+            "<>","").replace(
+            "()","")
         if edited == txt:
             return txt
         txt = edited
 
+######## Part 1 ##########
 def p1(expect=399153, viz=None):
     scores = []
+
     for line in DATA:
         trimmed = trim(line)
         for char in trimmed:
             if char in ERRVAL:
                 scores.append(ERRVAL[char])
-                break
+                break # get next line
+
     return sum(scores)
 
 ######## Part 2 ##########
-# line completer scores
-COMPVAL = {")": 1, "]": 2, "}": 3, ">": 4}
-
 def p2(expect=2995077699, viz=None):
     # calculate completer score for each incomplete line
     scores = []
+
     for line in DATA:
         txt = trim(line)
-        # find and discard corrupted strings
-        if any([char in txt for char in ERRVAL]):
+
+        # skip corrupted strings - anything containing bad closures
+        if set(txt).intersection(ERRVAL):
             continue
-        # reverse the string and make opens into closes
-        txt = txt[::-1].replace("{","}").replace("[","]").replace("<",">").replace("(",")")
+
+        # reverse the string and replace openers with closures
+        txt = txt[::-1].replace(
+               "{","}").replace(
+               "[","]").replace(
+               "<",">").replace(
+               "(",")")
+
         # calculate the score
         acc = 0
         for char in txt:
